00f6594a81a2892c54fe3afa936438898ce4e299
Emarat <emarathossain00@gmail.com>
2022-08-05 02:10:16 +0600

    Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
