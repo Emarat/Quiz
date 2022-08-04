9026b082acdbfdcf29eefb15fcad91bca8378d18
Emarat <emarathossain00@gmail.com>
2022-08-04 20:52:26 +0600

    Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="light">
+                <Container>
+                    <Navbar.Brand href="#home">Quiz</Navbar.Brand>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
