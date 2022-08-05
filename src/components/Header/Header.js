e5c52f791cb671ecc53cb24aaa18682eefc37d16
Emarat <emarathossain00@gmail.com>
2022-08-06 01:15:36 +0600

    toggle login/logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const [user, setUser] = useState({});
+    const auth = getAuth();
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate('/login')
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user)
+            }
+        });
+    }, [])
+
+    const handleSignOut = () => {
+        signOut(auth).then(() => {
+            setUser(null)
+        })
+
+    }
     return (
         <div>
             <Navbar bg="light">
                 <Container>
                     <Navbar.Brand href="/home">Quiz</Navbar.Brand>
+                    {
+                        user ?
+                            <Button onClick={handleSignOut} variant="light">{user.displayName}--Logout</Button>
+                            :
+                            <Button onClick={handleNavigate} variant="light">LOGIN</Button>
+                    }
                 </Container>
             </Navbar>
         </div>
